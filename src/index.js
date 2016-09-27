fd332d9d2f92898f54d9f3aaf5b1bdd40e165c10
Taylor Johnson <taylor.c.johnson@usace.army.mil>
2016-09-27 17:59:13 -0400

    Add debounce to videoSearch function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -6,7 +7,7 @@ import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCGgKvOMJvFp6HdKTFytBIywiV8sDAMXWw';
 
-class App extends Component {
+class App extends Component { // Class components when you need to manage state
   constructor(props) {
     super(props);
 
@@ -15,7 +16,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'mountain bike Florida Santos'}, (videos) => {
+    this.videoSearch('mountain bike Santos Florida');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -24,9 +29,10 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={ this.state.selectedVideo }/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
